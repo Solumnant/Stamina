dc8babb248865f330f1f9bea45b5a38b2cc78724
DPS <3ds.dps@gmail.com>
2021-03-31 17:09:44 -0500

    reorganize vertex declaration/type stuff

--- Stamina/src/rageformats/grcVertexFormat.h
+++ Stamina/src/rageformats/grcVertexFormat.h
@@ -2,7 +2,7 @@
 #define _GRC_VERTEXFORMAT_H
 #include <Xml.h>
 
-enum struct VertexElementTypes : int {
+enum VertexElementTypes : int {
     PositionType = 0, // D3DVERTEXELEMENT_FLOAT3
     BlendWeightType = 1, // D3DVERTEXELEMENT_D3DCOLOR
     BlendIndicesType = 2, // D3DVERTEXELEMENT_D3DCOLOR
@@ -21,20 +21,20 @@ enum struct VertexElementTypes : int {
     BinormalType = 15, // D3DVERTEXELEMENT_FLOAT4
 };
 
-enum struct D3DVertexElementType {
-    D3DVERTEXELEMENT_FLOAT16,
-    D3DVERTEXELEMENT_FLOAT16_2,
-    D3DVERTEXELEMENT_FLOAT16_3,
-    D3DVERTEXELEMENT_FLOAT16_4,
-    D3DVERTEXELEMENT_FLOAT1,
-    D3DVERTEXELEMENT_FLOAT2,
-    D3DVERTEXELEMENT_FLOAT3,
-    D3DVERTEXELEMENT_FLOAT4,
-    D3DVERTEXELEMENT_UBYTE4,
-    D3DVERTEXELEMENT_D3DCOLOR,
-    D3DVERTEXELEMENT_DEC3N,
-    D3DVERTEXELEMENT_NOTAVAILABLE = 0
+enum D3DVertexElementType {
+    D3DVERTEXELEMENT_NOTAVAILABLE = 0,
+    D3DVERTEXELEMENT_FLOAT16_2 = 1,
+    D3DVERTEXELEMENT_FLOAT1 = 2,
+    D3DVERTEXELEMENT_FLOAT16_4 = 3,
+    D3DVERTEXELEMENT_FLOAT16_3 = 4, //float unk?
+    D3DVERTEXELEMENT_FLOAT2 = 5,
+    D3DVERTEXELEMENT_FLOAT3 = 6,
+    D3DVERTEXELEMENT_FLOAT4 = 7,
+    D3DVERTEXELEMENT_UBYTE4 = 8,
+    D3DVERTEXELEMENT_D3DCOLOR = 9,
+    D3DVERTEXELEMENT_DEC3N = 10,
 };
+
 
 
 
@@ -53,27 +53,7 @@ public:
     unsigned long long GetFVF() {
         return m_vertexFields;
     }
-
-    void GetComponentOffset(size_t &offset, int bit, unsigned long long type) {
-        D3DVertexElementType elementType = (D3DVertexElementType)((type >> (4 * bit)) & 0xF);
-        switch (elementType) {
-            case D3DVertexElementType::D3DVERTEXELEMENT_FLOAT4: offset += 4;
-            case D3DVertexElementType::D3DVERTEXELEMENT_FLOAT3: offset += 4;
-            case D3DVertexElementType::D3DVERTEXELEMENT_FLOAT2: offset += 4;
-            case D3DVertexElementType::D3DVERTEXELEMENT_NOTAVAILABLE:
-            case D3DVertexElementType::D3DVERTEXELEMENT_FLOAT1:
-            case D3DVertexElementType::D3DVERTEXELEMENT_FLOAT16_3: offset += 4; //float unk?
-                break;
-            case D3DVertexElementType::D3DVERTEXELEMENT_DEC3N: offset += 4;
-                break;
-            case D3DVertexElementType::D3DVERTEXELEMENT_D3DCOLOR:
-            case D3DVertexElementType::D3DVERTEXELEMENT_UBYTE4: offset += 4;
-                break;
-            case D3DVertexElementType::D3DVERTEXELEMENT_FLOAT16_2: offset += 4;
-                break;
-        }
-    };
-    void GetComponentCountOffset(unsigned short &offset, int bit) {
+    void GetComponentCountOffset(int &offset, int bit) {
         D3DVertexElementType elementType = (D3DVertexElementType)((m_vertexFields >> (4 * bit)) & 0xF);
         switch (elementType) {
             case D3DVertexElementType::D3DVERTEXELEMENT_FLOAT4: offset += 4;
@@ -86,7 +66,7 @@ public:
                 break;
             case D3DVertexElementType::D3DVERTEXELEMENT_FLOAT1: offset += 1;
                 break;
-            case D3DVertexElementType::D3DVERTEXELEMENT_FLOAT16_3: offset += 3; //float unk?
+            case D3DVertexElementType::D3DVERTEXELEMENT_FLOAT16_3: offset += 0; //float unk?
                 break;
             case D3DVertexElementType::D3DVERTEXELEMENT_DEC3N: offset += 3;
                 break;
@@ -94,21 +74,10 @@ public:
                 break;
             case D3DVertexElementType::D3DVERTEXELEMENT_UBYTE4: offset += 4;
                 break;
-            case D3DVertexElementType::D3DVERTEXELEMENT_FLOAT16_2: offset += 4;
+            case D3DVertexElementType::D3DVERTEXELEMENT_FLOAT16_2: offset += 2;
                 break;
         }
     };
 
 };
 #endif
-//case VertexComponentType.Nothing: return 0;
-//case VertexComponentType.Half2: return 2;
-//case VertexComponentType.Float: return 1;
-//case VertexComponentType.Half4: return 4;
-//case VertexComponentType.FloatUnk: return 0;
-//case VertexComponentType.Float2: return 2;
-//case VertexComponentType.Float3: return 3;
-//case VertexComponentType.Float4: return 4;
-//case VertexComponentType.UByte4: return 4;
-//case VertexComponentType.Colour: return 4;
-//case VertexComponentType.Dec3N: return 3;
